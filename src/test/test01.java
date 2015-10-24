18938cdb6966cc8d17eede39eecf8708a5ec2131
Ryo_Shikata <smfshikaryo@gmail.com>
2015-10-24 22:04:27 +0900

    no message

--- src/test/test01.java
+++ src/test/test01.java
@@ -4,8 +4,11 @@ public class test01 {
 
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
-		
+
+		String str = "かきくけこ";
+
 		System.out.println("あいうえお");
+		System.out.println(str);
 
 	}
 
